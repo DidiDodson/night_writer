f823e59f67cc25f49c06d1fcd34161a440a0c051
Didi Dodson <mdodson55@gmail.com>
2021-09-24 09:36:41 -0600

    Feat: add first_method test

---
+++ spec/messages_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'optparse'
+# require './lib/night_writer'
+require './lib/messages'
+
+describe Messages do
+  it 'exists' do
+    message = Messages.new('braille.txt')
+
+    expect(message).to be_a(Messages)
+  end
+
+  it 'prints first message' do
+    message = Messages.new('braille.txt')
+
+    expect(message.first_message).to eq("Created 'braille.txt' containing 256 characters")
+  end
+
+  it 'reads number of characters' do
+    message = Messages.new('braille.txt')
+
+    expect(message.read_char_num).to
+  end
+end
