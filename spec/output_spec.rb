5b3e6895b6036ae8970c1e763c6e0fcb25e2ad05
Didi Dodson <mdodson55@gmail.com>
2021-09-24 11:34:50 -0600

    Feat: update with add_file method

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -5,8 +5,17 @@ require './lib/output'
 
 describe Output do
   it 'exists' do
-    output = Output.new
+    output = Output.new('dogs.txt')
 
     expect(output).to be_a(Output)
+  end
+
+  it "creates file" do
+    output = Output.new('dogs.txt')
+
+    output.add_file
+
+    expect(File.exists?'dogs.txt').to eq true
+    expect(File.exists? 'cats.txt').to eq false
   end
 end
