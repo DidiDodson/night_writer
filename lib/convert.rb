03a07fd36757e9596c650b290ac48c015bbda4fe
Didi Dodson <mdodson55@gmail.com>
2021-09-25 14:54:40 -0600

    Feat: refactor of dictionary to split

--- lib/convert.rb
+++ lib/convert.rb
@@ -1,6 +1,69 @@
-class Convert
-  attr_reader
+class Convert < WriterDictionary
+  attr_accessor :file_name, :dictionary, :arr1
 
-  def initialize
+  def initialize(file_name)
+    super
+  end
+
+  def translate
+    line1 = @arr1.map do |arr|
+      arr[0]
+
+    end
+
+    line2 = @arr1.map do |arr|
+      arr[1]
+    end
+
+    line3 = @arr1.map do |arr|
+      arr[2]
+    end
+
+    if @arr1.count <= 77
+      cond1 = line1[0..77]
+      cond2 = line2[0..77]
+      cond3 = line3[0..77]
+
+      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n"}
+
+    elsif (arr1.count >= 78) && (arr1.count < 155)
+      cond1 = line1[0..77]
+      cond2 = line2[0..77]
+      cond3 = line3[0..77]
+      cond4 = line1[78..155]
+      cond5 = line2[78..155]
+      cond6 = line3[78..155]
+
+      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n#{cond4.join(" ")}\n#{cond5.join(" ")}\n#{cond6.join(" ")}\n"}
+
+    elsif (arr1.count >= 156) && (arr1.count < 233)
+      cond1 = line1[0..77]
+      cond2 = line2[0..77]
+      cond3 = line3[0..77]
+      cond4 = line1[78..155]
+      cond5 = line2[78..155]
+      cond6 = line3[78..155]
+      cond7 = line1[156..233]
+      cond8 = line2[156..233]
+      cond9 = line3[156..233]
+
+      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n#{cond4.join(" ")}\n#{cond5.join(" ")}\n#{cond6.join(" ")}\n#{cond7.join(" ")}\n#{cond8.join(" ")}\n#{cond9.join(" ")}\n"}
+
+    elsif (arr1.count >= 234) && (arr1.count < 312)
+      cond1 = line1[0..77]
+      cond2 = line2[0..77]
+      cond3 = line3[0..77]
+      cond4 = line1[78..155]
+      cond5 = line2[78..155]
+      cond6 = line3[78..155]
+      cond7 = line1[156..233]
+      cond8 = line2[156..233]
+      cond9 = line3[156..233]
+      cond10 = line1[234..312]
+      cond11 = line2[234..312]
+      cond12 = line3[234..312]
+
+      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n#{cond4.join(" ")}\n#{cond5.join(" ")}\n#{cond6.join(" ")}\n#{cond7.join(" ")}\n#{cond8.join(" ")}\n#{cond9.join(" ")}\n#{cond10.join(" ")}\n#{cond11.join(" ")}\n#{cond12.join(" ")}\n"}
+    end
   end
 end
--- spec/convert_spec.rb
+++ spec/convert_spec.rb
@@ -2,13 +2,24 @@ require 'rspec'
 require 'optparse'
 require './lib/messages'
 require './lib/output'
-require './lib/dictionary'
-require './lib/process'
+require './lib/writer_dictionary'
+require './lib/convert'
 
 describe Convert do
   it 'exists' do
-    convert = Convert.new
+    convert = Convert.new('braille.txt')
 
     expect(convert).to be_a Convert
+  end
+
+  it 'converts to braille' do
+    convert = Convert.new('braille.txt')
+    writer_dictionary = WriterDictionary.new('braille.txt')
+
+    writer_dictionary.search_by_letter("e")
+    writer_dictionary.read
+    convert.translate
+
+    expect(convert).to eq(2)
   end
 end
