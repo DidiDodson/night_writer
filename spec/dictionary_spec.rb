0d6e682c86af4c95c83dd32ee3a0fb359744098d
Didi Dodson <mdodson55@gmail.com>
2021-09-25 10:19:29 -0600

    Feat: update to include file name

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -6,19 +6,19 @@ require './lib/dictionary'
 
 describe Dictionary do
   it 'exists' do
-    dictionary = Dictionary.new
+    dictionary = Dictionary.new('braille.txt')
 
     expect(dictionary).to be_a(Dictionary)
   end
 
   it 'has attributes' do
-    dictionary = Dictionary.new
+    dictionary = Dictionary.new('braille.txt')
 
     expect(dictionary.dict[:h]).to eq("0.\n00\n..")
   end
 
   it 'looks up letters' do
-     dictionary = Dictionary.new
+     dictionary = Dictionary.new('braille.txt')
 
      expected = "0.\n.0\n.."
 
@@ -26,7 +26,7 @@ describe Dictionary do
   end
 
   it 'reads english, converts' do
-    dictionary = Dictionary.new
+    dictionary = Dictionary.new('braille.txt')
 
     dictionary.read_conv
 
