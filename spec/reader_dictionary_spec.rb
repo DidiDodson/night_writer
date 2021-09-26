2965e742cd1b7c168cf15207e9922e562bce456b
Didi Dodson <mdodson55@gmail.com>
2021-09-25 21:55:28 -0600

    update test

--- spec/reader_dictionary_spec.rb
+++ spec/reader_dictionary_spec.rb
@@ -6,29 +6,37 @@ require './lib/reader_dictionary'
 
 describe ReaderDictionary do
   it 'exists' do
-    reader_dictionary = ReaderDictionary.new('braille.txt')
+    reader_dictionary = ReaderDictionary.new('braille0.txt')
 
     expect(reader_dictionary).to be_a(ReaderDictionary)
   end
 
   it 'has attributes' do
-    reader_dictionary = ReaderDictionary.new('braille.txt')
+    reader_dictionary = ReaderDictionary.new('braille0.txt')
 
     expect(reader_dictionary.r_dictionary[["0.","..",".."]]).to eq("a")
   end
 
   it 'looks up braille' do
-     reader_dictionary = ReaderDictionary.new('braille.txt')
+     reader_dictionary = ReaderDictionary.new('braille0.txt')
 
      expect(reader_dictionary.search_by_braille(["0.","..",".."])).to eq("a")
   end
 
   it 'reads English' do
-    reader_dictionary = ReaderDictionary.new('braille.txt')
+    reader_dictionary = ReaderDictionary.new('braille0.txt')
 
     reader_dictionary.read
 
     expect(reader_dictionary.read).to be_a(Array)
-    expect(reader_dictionary.read.count).to eq(45)
+  end
+
+  xit 'translates' do
+    reader_dictionary = ReaderDictionary.new('braille0.txt')
+
+    reader_dictionary.read
+    reader_dictionary.translate
+
+    expect(reader_dictionary.translate).to eq(true)
   end
 end
