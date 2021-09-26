a0c6c3c3e240fe3993b02a007db205c94b84d0d5
Didi Dodson <mdodson55@gmail.com>
2021-09-25 18:27:55 -0600

    Feat: add additional code for braille count

--- lib/messages.rb
+++ lib/messages.rb
@@ -17,9 +17,15 @@ class Messages
     text = lines.chomp!
     total_characters = text.length
     "#{total_characters}"
+
+  end
+
+  def read_braille_num
+    lines = IO.read("braille.txt")
+    (lines.split(" ").count) / 3
   end
 
   def second_message
-    "Created '#{file_name}' containing #{read_char_num} characters."
+    "Created '#{file_name}' containing #{read_braille_num} characters."
   end
 end
--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -20,4 +20,16 @@ describe Messages do
 
     expect(message.read_char_num).to eq("5")
   end
+
+  it 'prints second message' do
+    message = Messages.new('braille.txt')
+
+    expect(message.second_message).to eq("Created 'braille.txt' containing 45 characters.")
+  end
+
+  it 'reads braille letter count' do
+    message = Messages.new('braille.txt')
+
+    expect(message.read_braille_num).to eq(45)
+  end
 end
