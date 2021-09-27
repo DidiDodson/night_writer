29fa6883057bbad289d44278a926c3d3eadc0483
Didi Dodson <mdodson55@gmail.com>
2021-09-27 09:10:43 -0600

    Add external accessibility and remove translate methods

--- lib/writer_dictionary.rb
+++ lib/writer_dictionary.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 class WriterDictionary
-  attr_accessor :dictionary, :file_name, :arr1
+  attr_accessor :dictionary, :file_name
 
   def initialize(file_name)
-    @arr1 = []
     @file_name = file_name
     @dictionary = {
              a: "0.\n..\n..", b: "0.\n0.\n..", c: "00\n..\n..", d: "00\n.0\n..",
@@ -20,77 +19,7 @@ class WriterDictionary
            }
   end
 
-  def search_by_letter(input_item)
-    @dictionary[input_item.to_sym]
-  end
-
-  def read
-    lines = IO.read("messages.txt").chomp
-    lines.each_char do |line|
-      letter_hash = search_by_letter(line)
-      @arr1 << letter_hash.split("\n")
-    end
-
-    @line1 = @arr1.map do |arr|
-      arr[0]
-    end
-
-    @line2 = @arr1.map do |arr|
-      arr[1]
-    end
-
-    @line3 = @arr1.map do |arr|
-      arr[2]
-    end
-  end
-
-  def translate
-
-    if @arr1.count <= 77
-      cond1 = @line1[0..77]
-      cond2 = @line2[0..77]
-      cond3 = @line3[0..77]
-
-      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n"}
-
-    elsif (arr1.count >= 78) && (arr1.count < 155)
-      cond1 = line1[0..77]
-      cond2 = line2[0..77]
-      cond3 = line3[0..77]
-      cond4 = line1[78..155]
-      cond5 = line2[78..155]
-      cond6 = line3[78..155]
-
-      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n#{cond4.join(" ")}\n#{cond5.join(" ")}\n#{cond6.join(" ")}\n"}
-
-    elsif (arr1.count >= 156) && (arr1.count < 233)
-      cond1 = line1[0..77]
-      cond2 = line2[0..77]
-      cond3 = line3[0..77]
-      cond4 = line1[78..155]
-      cond5 = line2[78..155]
-      cond6 = line3[78..155]
-      cond7 = line1[156..233]
-      cond8 = line2[156..233]
-      cond9 = line3[156..233]
-
-      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n#{cond4.join(" ")}\n#{cond5.join(" ")}\n#{cond6.join(" ")}\n#{cond7.join(" ")}\n#{cond8.join(" ")}\n#{cond9.join(" ")}\n"}
-
-    elsif (arr1.count >= 234) && (arr1.count < 312)
-      cond1 = line1[0..77]
-      cond2 = line2[0..77]
-      cond3 = line3[0..77]
-      cond4 = line1[78..155]
-      cond5 = line2[78..155]
-      cond6 = line3[78..155]
-      cond7 = line1[156..233]
-      cond8 = line2[156..233]
-      cond9 = line3[156..233]
-      cond10 = line1[234..312]
-      cond11 = line2[234..312]
-      cond12 = line3[234..312]
-
-      File.open(@file_name, mode: "a"){|file| file.puts "#{cond1.join(" ")}\n#{cond2.join(" ")}\n#{cond3.join(" ")}\n#{cond4.join(" ")}\n#{cond5.join(" ")}\n#{cond6.join(" ")}\n#{cond7.join(" ")}\n#{cond8.join(" ")}\n#{cond9.join(" ")}\n#{cond10.join(" ")}\n#{cond11.join(" ")}\n#{cond12.join(" ")}\n"}
-    end
+  class << self
+    attr_accessor :dictionary
   end
 end
