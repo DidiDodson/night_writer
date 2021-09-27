320732c9a4f9458ff9115115922e05940e291b38
Didi Dodson <mdodson55@gmail.com>
2021-09-27 09:11:22 -0600

    Update to include translate class and remove dictionary class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,20 +3,20 @@ require 'optparse'
 require './messages'
 require './output'
 require './writer_dictionary'
+require './w_translate'
 
 @f_message = ARGV[0]
 @s_message = ARGV[1]
 
 message1 = Messages.new(@s_message)
 output1 = Output.new(@s_message)
-writer_dictionary1 = WriterDictionary.new(@s_message)
-
+translate1 = Translate.new(@s_message)
 
 puts message1.first_message
 output1.add_file
-writer_dictionary1.search_by_letter(@s_message)
-writer_dictionary1.read
-writer_dictionary1.translate
+translate1.search_by_letter(@s_message)
+translate1.read
+translate1.translate
 
 
 # ./night_writer.rb messages.txt braille.txt
