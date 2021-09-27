714eb719b0841ce1e741c665b85b6b699debe73b
Didi Dodson <mdodson55@gmail.com>
2021-09-27 09:10:25 -0600

    Remove translate methods testing

--- spec/writer_dictionary_spec.rb
+++ spec/writer_dictionary_spec.rb
@@ -16,28 +16,4 @@ describe WriterDictionary do
 
     expect(writer_dictionary.dictionary[:h]).to eq("0.\n00\n..")
   end
-
-  it 'looks up letters' do
-     writer_dictionary = WriterDictionary.new('tester.txt')
-
-     expected = "0.\n.0\n.."
-
-     expect(writer_dictionary.search_by_letter("e")).to eq(expected)
-  end
-
-  it 'reads English' do
-    writer_dictionary = WriterDictionary.new('tester.txt')
-
-    writer_dictionary.read
-
-    expect('messages.txt'.length).to eq(12)
-  end
-
-  it 'translates to Braille' do
-    writer_dictionary = WriterDictionary.new('tester.txt')
-
-    writer_dictionary.translate
-
-    expect('tester.txt'.length).to eq(10)
-  end
 end
