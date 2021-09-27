01910fd6c8938f3783a5f47a9958d6ce71e8552e
Didi Dodson <mdodson55@gmail.com>
2021-09-27 15:39:49 -0600

    Update test to include second argument

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -1,21 +1,21 @@
 require 'rspec'
-require 'optparse'
 require './lib/messages'
 require './lib/output'
 
 describe Output do
   it 'exists' do
-    output = Output.new('dogs.txt')
+    output = Output.new('dogs.txt', 'cats.txt')
 
     expect(output).to be_a(Output)
   end
 
   it "creates file" do
-    output = Output.new('dogs.txt')
+    output = Output.new('dogs.txt', 'cats.txt')
 
-    output.add_file
+    output.add_file_1
+    output.add_file_2
 
     expect(File.exists?'dogs.txt').to eq true
-    expect(File.exists? 'cats.txt').to eq false
+    expect(File.exists? 'fish.txt').to eq false
   end
 end
