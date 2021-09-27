442573c58e63bc7bcf813f0219df2a6e45bfa33f
Didi Dodson <mdodson55@gmail.com>
2021-09-27 08:20:31 -0600

    Update to make more portable

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -4,19 +4,19 @@ require './messages'
 require './output'
 require './writer_dictionary'
 
-f_message = ARGV[0]
-s_message = ARGV[1]
+@f_message = ARGV[0]
+@s_message = ARGV[1]
 
-message1 = Messages.new(s_message)
-output1 = Output.new(s_message)
-writer_dictionary1 = WriterDictionary.new(s_message)
+message1 = Messages.new(@s_message)
+output1 = Output.new(@s_message)
+writer_dictionary1 = WriterDictionary.new(@s_message)
 
 
 puts message1.first_message
 output1.add_file
-writer_dictionary1.search_by_letter(s_message)
+writer_dictionary1.search_by_letter(@s_message)
 writer_dictionary1.read
 writer_dictionary1.translate
 
 
-# ./night_writer0.rb messages.txt tester.txt
+# ./night_writer.rb messages.txt braille.txt
