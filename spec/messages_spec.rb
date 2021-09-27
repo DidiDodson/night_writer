295f03efb6cc78a27605e6caa2b8c413081e6d55
Didi Dodson <mdodson55@gmail.com>
2021-09-27 12:49:02 -0600

    Updated messages spec to add testing

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -1,36 +1,37 @@
 require 'rspec'
 require 'optparse'
 require './lib/messages'
+require './braille_test.rb'
 
 describe Messages do
+
   it 'exists' do
-    message = Messages.new('braille0.txt')
+    message = Messages.new(:test_file)
 
     expect(message).to be_a(Messages)
   end
 
+  it 'reads number of english characters' do
+    message = Messages.new(:test_file)
+
+    expect(message.read_char_num).to eq("13")
+  end
+
   it 'prints first message' do
-    message = Messages.new('braille0.txt')
+    message = Messages.new(:test_file)
 
-    expect(message.first_message).to eq("Created 'braille0.txt' containing 5 characters")
-  end
-
-  it 'reads number of characters' do
-    message = Messages.new('braille0.txt')
-
-    expect(message.read_char_num).to eq("5")
-  end
-
-  it 'prints second message' do
-    message = Messages.new('braille0.txt')
-
-    expect(message.second_message).to eq("Created 'braille0.txt' containing 22 characters.")
-require "pry"; binding.pry
+    expect(message.first_message).to eq("Created 'test_file' containing 13 characters")
   end
 
   it 'reads braille letter count' do
-    message = Messages.new('braille0.txt')
+    message = Messages.new(:test_file)
 
-    expect(message.read_braille_num).to eq(68)
+    expect(message.read_braille_num).to eq(6)
+  end
+
+  it 'prints second message' do
+    message = Messages.new(:test_file)
+
+    expect(message.second_message).to eq("Created 'test_file' containing 6 characters.")
   end
 end
